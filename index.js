fdcc83014edf4f1d156341fb549c72431d73b4c2
sera <120779389+seraphimes@users.noreply.github.com>
2024-09-17 01:43:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = { name, email };
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+    return fetch("http://localhost:3000/users", configObj)
+      .then(resp => resp.json())
+      .then(data => {
+        document.body.innerHTML += `<p>${data.id}</p>`;
+      })
+      .catch(error => {
+        document.body.innerHTML += `<p>${error.message}</p>`;
+      });
+  }
+
+  
